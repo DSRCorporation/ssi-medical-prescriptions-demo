9464dabe81f70d8e04a391f7a7a395772a41d546
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-07-07 16:09:01 +0500

    There should be requestId instead of presentationId

--- internal/storage/pharmacy_storage_interface.go
+++ internal/storage/pharmacy_storage_interface.go
@@ -21,7 +21,7 @@
 package storage
 
 type PharmacyStorage interface {
-	CreatePresentationRequest(pharmacyId string, presentationId string) (err error)
+	CreatePresentationRequest(pharmacyId string, requestId string) (err error)
 	GetPharmacyIdByRequestId(requestId string) (pharmacyId string, err error)
 	GetPresentationIdByRequestId(requestId string) (presentationId string, err error)
 	GetPresentationIdsByPharmacyId(pharmacyId string) (presentationIds []string, err error)
